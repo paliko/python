4bf56f49dc3d4167d0b945f5bebbb56aeedfcdc8
paliko <paliko@centrum.cz>
2023-08-23 13:35:25 +0200

    added pr 15 threding.Thread

--- gopas_skoleni/day3/pr15.py
+++ gopas_skoleni/day3/pr15.py
@@ -13,7 +13,12 @@ vlakno1 = threading.Thread(target=citac,args=(1,)) # vytvori vlakno z funkce
                                                     # kdyby tam nebyla carka (1,) tak by to byl int a ne ntice
 vlakno2 = threading.Thread(target=citac,args=(2,)) 
 
+vlakno1.deamon = True
+vlakno2.deamon = True
+
 vlakno1.start()
 vlakno2.start()
+
+
 
 print ("Pokracuje dal .....")
