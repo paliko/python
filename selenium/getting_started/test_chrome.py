e4f899ce5680b320986b7dd509e95a413cad6458
paliko <paliko@centrum.cz>
2024-01-23 21:41:43 +0100

    vytvoreni folder - react zaklady

--- selenium/getting_started/test_chrome.py
+++ selenium/getting_started/test_chrome.py
@@ -3,6 +3,7 @@ from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.common.keys import Keys
 
 options = Options()
 options.add_argument('--headless')
@@ -10,7 +11,12 @@ options.add_argument('--no-sandbox')
 options.add_argument('--disable-dev-shm-usage')
 driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
 
-# driver.get("https://python.org")
-driver.get("https://example.com")
+driver.get("https://python.org")
+# driver.get("https://example.com")
 print(driver.title)
+search_bar = driver.find_element_by_name("q")
+search_bar.clear()
+search_bar.send_keys("getting started with python")
+search_bar.send_keys(Keys.RETURN)
+print(driver.current_url)
 driver.close()
